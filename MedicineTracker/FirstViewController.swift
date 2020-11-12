718f3359807ce3767821f78acc952e54a6896064
Omar Ahmad <superomar121@gmail.com>
2020-11-12 00:52:48 -0800

    Initial Commit

---
+++ MedicineTracker/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MedicineTracker
+//
+//  Created by Omar Ahmed on 11/12/20.
+//  Copyright © 2020 Omar Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
