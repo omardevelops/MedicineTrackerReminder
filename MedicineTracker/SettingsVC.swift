b0ab352c0c6a2dfa49c6212197b3c735479db8e6
Omar Ahmad <superomar121@gmail.com>
2020-11-12 01:13:01 -0800

    Initial commit

---
+++ MedicineTracker/SettingsVC.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsVC.swift
+//  MedicineTracker
+//
+//  Created by Omar Ahmed on 11/12/20.
+//  Copyright © 2020 Omar Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
