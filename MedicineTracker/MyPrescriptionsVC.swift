a6a4f69b0f2bc6245e2b97306f6c75cbf7440e8c
Omar Ahmad <superomar121@gmail.com>
2020-11-25 04:29:17 -0800

    Added core data context + Removed extensions, not needed

--- MedicineTracker/MyPrescriptionsVC.swift
+++ MedicineTracker/MyPrescriptionsVC.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 
-class MyPrescriptionsVC: UIViewController {
+class MyPrescriptionsVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // MARK: Core Data Context
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
-
-    static var cellCount = 11;
+    var myPrescriptions : [Prescription]?
+    static var cellCount = 11
 
     @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.register(prescriptionCell.nib(), forCellWithReuseIdentifier: "prescriptionCell")
+        
         
         collectionView.delegate = self
         collectionView.dataSource = self
@@ -25,23 +30,14 @@ class MyPrescriptionsVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-
-}
-
-extension MyPrescriptionsVC: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
-        print("Tapped")
+        print("Tapped Cell #", indexPath.row)
     }
-   
-    
-}
-
-extension MyPrescriptionsVC: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection: Int) -> Int {
-        return MyPrescriptionsVC.cellCount;
+        //return myPrescriptions?.count ?? 0
+        return MyPrescriptionsVC.cellCount
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -51,11 +47,42 @@ extension MyPrescriptionsVC: UICollectionViewDataSource {
         return cell
     }
     
+    // MARK: Fetch Prescriptions
+    func fetchPrescriptions() {
+        do {
+            self.myPrescriptions = try self.context.fetch(Prescription.fetchRequest())
+            
+            DispatchQueue.main.async {
+                // Reload CollectionView
+                self.collectionView.reloadData()
+            }
+        }
+        catch {
+            // TODO: Handle Error Here
+        }
+    }
+
+
+}
+
+/* MARK: Not needed
+extension MyPrescriptionsVC: UICollectionViewDelegate {
+    
+    
    
     
 }
 
-extension MyPrescriptionsVC: UICollectionViewDelegateFlowLayout{
+extension MyPrescriptionsVC: UICollectionViewDataSource {
+    
+    
+    
+   
     
 }
+
+
+extension MyPrescriptionsVC: UICollectionViewDelegateFlowLayout{
+    
+}*/
 
